c9fbd898eab78c58e33abed6a1a25f3e1b4ad206
Cristian Parra <cristianpar.2002@gmail.com>
2025-01-27 09:01:40 -0500

    Configuracion assets

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // Asegura que las rutas de los recursos sean relativas
+  build: {
+    outDir: 'dist', // Carpeta donde se generará la build
+    assetsDir: 'assets', // Carpeta donde se colocarán los recursos estáticos
+  },
+  resolve: {
+    alias: {
+      // Configuración opcional para alias
+      '@': '/src',
+    },
+  },
+});
